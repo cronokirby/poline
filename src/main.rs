196eb8857c0eec1b0821cb02cf8dd4318357d5a7
Lúcás Meier <cronokirby@gmail.com>
2019-07-27 14:14:38 -0400

    Add a Token enum
    This enum will be used to represent the tokens in our language, and is
    the first step in writing the lexer.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+/// Represents the Tokens that compose the language.
+///
+/// The first step in going from a textual representation of the language to
+/// an in-code representation is to split up the text into a sequence of
+/// tokens. Besides the litteral and string tokens, each token corresponds
+/// to a single string. The variable tokens have specific content, either
+/// because they represent a string litteral, or a variable name.
+enum Token {
+    /// Declares a function; corresponds to `fn`.
+    Function,
+    /// Starts a receive statement; corresponds to `recv`.
+    Recv,
+    /// Starts a send statement; corresponds to `send`.
+    Send,
+    /// Used in the latter part of a send statement; corresponds to `to`.
+    To,
+    /// Starts a spawn statement; corresponds to `spawn`.
+    Spawn,
+    /// Used in the latter part of a spawn statement; corresponds to `as`.
+    As,
+    /// Used at the start of a print statement; corresponds to `print`.
+    Print,
+    /// Represents the first brace in a function; corresponds to `{`.
+    OpenBrace,
+    /// Represents the last brace in a function; corresponds to `}`.
+    CloseBrace,
+    /// Represents the end of a statement; corresponds to `;`.
+    Semicolon,
+    /// Represents a string litteral; e.g. `"abc"`, `"bar"`.
+    Str(String),
+    /// Represents a variable; e.g. `x`, `baz`.
+    Name(String),
+}
+
 fn main() {
     println!("Hello, world!");
 }
