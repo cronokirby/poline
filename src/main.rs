0b2d08980f0e1e963e14c6f4d5ed2df5a949d379
Lúcás Meier <cronokirby@gmail.com>
2019-07-27 15:25:28 -0400

    Add lexing for braces

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::str::Chars;
+
 /// Represents the Tokens that compose the language.
 ///
 /// The first step in going from a textual representation of the language to
@@ -5,6 +7,7 @@
 /// tokens. Besides the litteral and string tokens, each token corresponds
 /// to a single string. The variable tokens have specific content, either
 /// because they represent a string litteral, or a variable name.
+#[derive(Debug, PartialEq)]
 enum Token {
     /// Declares a function; corresponds to `fn`.
     Function,
@@ -32,6 +35,51 @@ enum Token {
     Name(String),
 }
 
+/// Represents the type of errors that can occurr while lexing.
+#[derive(Debug, PartialEq)]
+enum LexError {}
+
+type LexResult = Result<Token, LexError>;
+
+/// The lexer takes in a source string, and spits out tokens.
+struct Lexer<'a> {
+    /// The source holds the entire program in a single string.
+    chars: Chars<'a>,
+}
+
+impl<'a> Lexer<'a> {
+    fn new(source: &'a str) -> Self {
+        Lexer {
+            chars: source.chars(),
+        }
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = LexResult;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.chars.next() {
+            None => None,
+            Some('{') => Some(Ok(Token::OpenBrace)),
+            Some('}') => Some(Ok(Token::CloseBrace)),
+            _ => None,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn lexer_works_for_braces() {
+        let text = "{}";
+        let tokens: Vec<LexResult> = Lexer::new(text).collect();
+        assert_eq!(tokens, vec![Ok(Token::OpenBrace), Ok(Token::CloseBrace)]);
+    }
 }
